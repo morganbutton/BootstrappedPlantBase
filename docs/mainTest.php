ff9ac9e37ac810ea167bcbe36b4ef27ad6b082e1
DESKTOP-65PKKFE\morga <morgan button>
2021-09-28 01:19:03 -0400

    working search bar test

--- docs/mainTest.php
+++ docs/mainTest.php
@@ -1,17 +1,27 @@
 <?php include 'phpScripts/universalScripts/head.php'?>
 
 <?php include 'phpScripts/universalScripts/header.php'?>
- 
+<form class="form-inline" role="search">
+            <input class="search js-search form-control form-control-rounded mr-sm-2" type="text" name="search" title="Enter search query here.." placeholder="Search.." aria-label="Search">
+</form>
 <?php
+      $search = $_GET['search'] ?? '1';;
+      
+     
+      
+      
       require_once 'phpinitializelogin/initialize.php';
-      $query = "SELECT * FROM books WHERE TITLE LIKE '%Euclid%'";
+      $query = "SELECT * FROM books WHERE TITLE LIKE '%$search%'";
       $result = $connection->query($query);
       if(!$result) die('Fatal Error');
       $rows = $result -> num_rows;
+       
+      
       for($j = 0; $j < $rows; ++$j){
         $row = $result->fetch_array(MYSQLI_ASSOC);
         echo '<b>'. htmlspecialchars($row['title']) . ' (' .htmlspecialchars($row['year']) .')' .'</b>'  .'<br>';
       }
+
 
 ?>
 //
